b1b728c51a081293cb85e463f7793f4fd8de5c4a
jessalbarian <albarian.jess@gmail.com>
2016-04-18 23:44:11 -0600

    added proposal

---
+++ Project3/app/src/main/java/jessie/project3/MainActivity.java
@@ -0,0 +1,31 @@
+package jessie.project3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    void mainCallBtn(){
+        /*
+        Add function for main call:
+        - get # for campus security
+        - call
+         */
+    }
+
+    void call911(){
+        /*
+        Add function for 911 call
+        - add alert
+        - call 911
+         */
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
